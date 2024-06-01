08c7ca5b1ba344455dc5eb7833253dfff2ec0736
Lokesh Khandelwal <lokeshkhandelwal2001@gmail.com>
2024-06-02 03:40:37 +0530

    Day 95

---
+++ 3110.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int scoreOfString(string s)
+{
+   int res = 0;
+   for (int i = 1; i < s.size(); i++)
+   {
+      res += abs(s[i] - s[i - 1]);
+   }
+   return res;
+}
+int main()
+{
+   cout << scoreOfString("hello")<<endl;
+   return 0;
+}
